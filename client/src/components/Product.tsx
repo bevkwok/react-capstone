2eeae5a6a9f5b273dd669c6ad11ea554f769d58c
Beverly Kwok <yan.kwok.b@gmail.com>
2021-09-23 12:28:36 -0700

    Create product and rating component to render a single product in product page

---
+++ client/src/components/Product.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Rating from './Rating'
+
+interface Prop {
+    product: Product
+}
+
+const Product: React.FC<Prop> = ({ product }) => {
+    return (
+        <div key={product._id} className="card">
+            <a href={`/product/${product._id}`}>
+                <img className="img-medium" src={product.image} alt={product.name} />
+            </a>
+            <div className="card-body">
+                <a href={`/product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                </a>
+                <div className="price">${product.price}</div>
+                <Rating rating={product.rating} numReviews={product.numReviews}/>
+            </div>
+        </div>
+    )
+}
+
+
+export default Product
