d087f723e5d294696e6c6139fbe6db7a73a59891
Beverly Kwok <yan.kwok.b@gmail.com>
2021-09-26 20:32:29 -0700

    Start Cart Page and route

--- client/src/Body.tsx
+++ client/src/Body.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import CartPage from './pages/CartPage';
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
 
@@ -19,8 +20,9 @@ function Body() {
                         </div>
                     </header>
                     <main>
-                    <Route path="/" component={HomePage} exact></Route>
+                    <Route path="/cart/:id?" component={CartPage} exact></Route>
                     <Route path="/product/:id" component={ProductPage} exact></Route>
+                    <Route path="/" component={HomePage} exact></Route>
                     </main>
                     <footer className="row center">All right reserved</footer>
                 </div>
