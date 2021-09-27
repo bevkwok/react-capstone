15541e5c76ca9e2d2746e8badd422016c3e0569a
Beverly Kwok <yan.kwok.b@gmail.com>
2021-09-27 15:40:45 -0700

    Changing anchor to Link

--- client/src/Body.tsx
+++ client/src/Body.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartPage from './pages/CartPage';
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
@@ -12,11 +12,11 @@ function Body() {
                 <div className="grid-container">
                     <header className="row">
                         <div>
-                        <a className="brand" href="/"><i className="fas fa-heart"></i>Shopaholic</a>
+                        <Link className="brand" to="/"><i className="fas fa-heart"></i>Shopaholic</Link>
                         </div>
                         <div>
-                        <a className="header-link" href="cart.html">Cart</a>
-                        <a className="header-link" href="signin.html">Sign In</a>
+                        <Link className="header-link" to="/cart">Cart</Link>
+                        <Link className="header-link" to="/signin">Sign In</Link>
                         </div>
                     </header>
                     <main>
