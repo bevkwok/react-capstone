9315a3b963ae78c7202f1232a49984d20670cb3b
Beverly Kwok <yan.kwok.b@gmail.com>
2021-09-26 21:31:08 -0700

    Edit productController and productRoute to provide product details information to product details page

--- server/src/controllers/productController.ts
+++ server/src/controllers/productController.ts
@@ -3,11 +3,23 @@ import data from '../productData'
 
 const getProducts = async(req: Request, res: Response): Promise<void> => {
     try {
-        const products = data.products;
-        res.status(200).json({ products })
+        res.status(200).json(data.products)
     } catch (error) {
         throw error
     }
 }
 
-export { getProducts }
+const getProduct = async(req: Request, res: Response): Promise<void> => {
+    try {
+        const product = data.products.find(product => product._id === req.params.id);
+        if (product) {
+            res.status(200).json(product)
+        } else {
+            res.status(404).send("Product not found") 
+        }
+    } catch (error) {
+        throw error
+    }
+}
+
+export { getProducts, getProduct }
--- server/src/routes/productRouter.ts
+++ server/src/routes/productRouter.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getProducts } from '../controllers/productController'
+import { getProducts, getProduct } from '../controllers/productController'
 
 const router = express.Router()
 
 router.get("/api/products", getProducts)
+router.get("/api/product/:id", getProduct)
+
 
 export { router as productRouter }
