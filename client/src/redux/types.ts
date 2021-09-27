d63b35e01e7e36ce3b8e78bc66743989f5015ac2
Beverly Kwok <yan.kwok.b@gmail.com>
2021-09-26 19:36:21 -0700

    Create action and action types for product details page

---
+++ client/src/redux/types.ts
@@ -0,0 +1,20 @@
+//user reducer types
+export const SET_AUTHENTICATED = "SET_AUTHENTICATED"
+export const SET_UNAUTHENTICATED = "SET_UNAUTHENTICATED"
+export const SET_USER = "SET_USER"
+export const LOADING_USER = "LOADING_USER"
+
+//UI reducer types
+export const SET_ERRORS = "SET_ERRORS"
+export const LOADING_UI = "LOADING_UI"
+export const CLEAR_ERRORS = "CLEAR_ERRORS"
+
+// Product list reducer types
+export const PRODUCT_LIST_REQUEST = "PRODUCT_LIST_REQUEST"
+export const PRODUCT_LIST_SUCCESS = "PRODUCT_LIST_SUCCESS"
+export const PRODUCT_LIST_FAIL = "PRODUCT_LIST_FAIL"
+
+// Product reducer types
+export const PRODUCT_DETAILS_REQUEST = "PRODUCT_DETAILS_REQUEST"
+export const PRODUCT_DETAILS_SUCCESS = "PRODUCT_DETAILS_SUCCESS"
+export const PRODUCT_DETAILS_FAIL = "PRODUCT_DETAILS_FAIL"
