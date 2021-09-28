9bcd9d8f1c2ec0731cabdff36792eb1b1d4a0ad9
Beverly Kwok <yan.kwok.b@gmail.com>
2021-09-28 11:11:01 -0700

    Apply signin action to signin page, add error and loading box

--- client/src/pages/SigninPage.tsx
+++ client/src/pages/SigninPage.tsx
@@ -1,21 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { signin } from '../redux/actions/userActions';
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
 
-const SigninPage = () => {
+const SigninPage = (props: any) => {
 
     const [username, setUsername]:[any, SetUsername] = useState('');
     const [password, setPassword]:[any, SetPassword] = useState('');
 
+    const redirect = props.location.search? props.location.search.split('=')[1]: '/'
+ 
+    const userSignin = useSelector((state: any) => state.userSignin )
+    const { userInfo, loading, error } = userSignin
+
+    const dispatch = useDispatch()
     const submitHandler = (e: any) => {
         e.preventDefault()
-        // signin action
+        dispatch(signin(username, password))
     }
+
+    useEffect(() => {
+        if(userInfo) {
+            props.history.push(redirect)
+        }
+    }, [props.history, redirect, userInfo])
     return (
         <div>
             <form className="form" onSubmit={submitHandler}>
                 <div>
                     <h1>Sign In</h1>
                 </div>
+                {loading && <LoadingBox></LoadingBox>}
+                {error && <MessageBox variant="error" message={error}/>}
                 <div>
                     <label htmlFor="username">Username</label>
                     <input type="text" id="username" placeholder="Enter username" required onChange={(e: any) => setUsername(e.target.value)} />
