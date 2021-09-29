97225014e23b43c59c88b958b257e2c2ffe6f802
Beverly Kwok <yan.kwok.b@gmail.com>
2021-09-28 19:01:43 -0700

    Add 3 actions (empty cart, save address, save payment method)

--- client/src/redux/reducers/cartReducers.ts
+++ client/src/redux/reducers/cartReducers.ts
@@ -1,8 +1,9 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../types";
-
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, EMPTY_CART, CART_SAVE_SHIPPING_ADDRESS, CART_SAVE_PAYMENT_METHOD } from "../types";
 
 const initialState: CartItemState = {
-    cartItems: localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems') || ' ' ) : []
+    cartItems: localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems') || '' ) : [],
+    shippingAddress: localStorage.getItem('shippingAddress')? JSON.parse(localStorage.getItem('shippingAddress') || '' ) : {},
+    paymentMethod: 'Stripe'
 };
 
 const cartReducer = (state = initialState, action: any) => {
@@ -29,6 +30,12 @@ const cartReducer = (state = initialState, action: any) => {
             }
         case CART_REMOVE_ITEM:
             return { ...state, cartItems: state.cartItems?.filter( cItem => cItem.product !== action.payload)}
+        case EMPTY_CART:
+            return state
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return { ...state, shippingAddress: action.payload}
+        case CART_SAVE_PAYMENT_METHOD:
+            return { ...state, paymentMethod: action.payload };
         default:
             return state;
     }
